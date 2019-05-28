d8ac24da4d057ab44cbc672913dd6a3450876a3d
ZALABIA <41113180+iiUMeex@users.noreply.github.com>
2019-05-28 14:55:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1773,5 +1773,15 @@ client.on('message', message => {
       
                           }
   });
+
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const xkiller = member.guild.channels.find("name", "chat");
+     xkiller.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+  });
+});
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
